6a8f5850b80ea12d488df87a5353b12328ca0d97
eric neil <neil.eric.0101@gmail.com>
2021-06-03 20:43:56 -0500

    remove footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import html from "html-literal";
-import logo from "../Images/IMG_0710.jpg"
+// import logo from "../Images/IMG_0710.jpg"
 export default () => html`
 
-<div class="Quencher"><img src=${logo}></div>`;
+<!-- <div class="Quencher"><img src=${logo}></div> -->
+`;
