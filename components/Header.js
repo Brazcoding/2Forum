19b4206955f80592a271b6c97c91940a70eb7fed
eric neil <neil.eric.0101@gmail.com>
2021-06-29 20:05:17 -0500

    changed title

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import html from "html-literal";
 
 export default () => html`
     <header class="Header101">
-        <div class="brand">Get An Grip</div>
+        <div class="brand">Get A Grip</div>
     </header>
 
 `;
